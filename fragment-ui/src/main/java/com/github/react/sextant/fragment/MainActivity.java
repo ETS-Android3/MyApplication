db2b4ab64072bde49c553153af616a9d34c18a33
罗坤 <1498710037@qq.com>
2019-09-28 16:30:00 +0800

    Learning Fragment UI

---
+++ fragment-ui/src/main/java/com/github/react/sextant/fragment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.react.sextant.fragment;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.total_view);
+    }
+}
