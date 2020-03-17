8fff22ead89036b6715232509c2900b45acfaf68
罗坤 <1498710037@qq.com>
2020-03-17 18:12:59 +0800

    代码暂存回家弄

--- aria-download/src/main/java/com/github/arialyy/ariatest/MainActivity.java
+++ aria-download/src/main/java/com/github/arialyy/ariatest/MainActivity.java
@@ -2,97 +2,117 @@ package com.github.arialyy.ariatest;
 
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.arialyy.annotations.Download;
 import com.arialyy.aria.core.Aria;
-import com.arialyy.aria.core.download.DownloadTask;
+import com.arialyy.aria.core.task.DownloadGroupTask;
+import com.arialyy.aria.core.task.DownloadTask;
+import com.arialyy.aria.util.RecordUtil;
+
+import static com.arialyy.aria.util.CheckUtil.checkDPathConflicts;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String DOWNLOAD_URL =
             "http://static.gaoshouyou.com/d/d4/4f/d6d48db3794fb9ecf47e83c346570881.apk";
-    final View view = getLayoutInflater().inflate(R.layout.activity_main, null);
-    TextView mSize = (TextView) view.findViewById(R.id.size);
-    TextView mSpeed = (TextView) view.findViewById(R.id.speed);
-
-    HorizontalProgressBarWithNumber mPb = (HorizontalProgressBarWithNumber) view.findViewById(R.id.progressBar);
+    long taskId;
+    View main;
+    HorizontalProgressBarWithNumber mPb;
+    TextView mSize;
+    TextView mSpeed;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-//        Aria.download(this).register();
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main2);
 
+        //注册aria
+        Aria.download(this).register();
+        Aria.get(this).getDownloadConfig().setConvertSpeed(true);
 
-        // 开始
-        view.findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Aria.download(this)
+        main = getLayoutInflater().inflate(R.layout.activity_main2, null);
+        mPb = (HorizontalProgressBarWithNumber) main.findViewById(R.id.progressBar);
+        mSize = (TextView) main.findViewById(R.id.size);
+        mSpeed = (TextView) main.findViewById(R.id.speed);
+
+    }
+
+    public void onClick(View v){
+        if(v.getId()==R.id.start){
+            //创建任务
+            String filePath = Environment.getExternalStorageDirectory().getPath() + "/aria_test.apk";
+//            System.out.println("luokun"+checkDPathConflicts(false,filePath));
+//            int state = Aria.download(this)
+//                    .load(taskId).getTaskState();
+//            System.out.println("luokun state"+state);
+//            if(state == 3){
+//                Aria.download(this)
+//                        .load(taskId)
+//                        .reStart();
+//            }
+
+            if(checkDPathConflicts(false,filePath)){
+                taskId = Aria.download(this)
                         .load(DOWNLOAD_URL)
-                        .setDownloadPath(Environment.getExternalStorageDirectory().getPath() + "/aria_test.apk")
-                        .start();
-
+                        .setFilePath(filePath) //设置文件保存的完整路径
+                        .create();
+            }else {
+                Aria.download(this)
+                        .load(RecordUtil.getTaskRecord())
+                        .resume();
             }
-        });
 
-        //暂停
-        view.findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+        }else if(v.getId()==R.id.stop){
+
+            Aria.download(this)
+                    .load(taskId)
+                    .stop();
+
+        }else if(v.getId()==R.id.cancel){
+            Aria.download(this)
+                    .load(taskId)
+                    .cancel();
+        }
+    }
+
+    //在这里处理任务执行中的状态，如进度进度条的刷新
+    @Download.onTaskRunning protected void running(DownloadTask task) {
+        if(task.getKey().equals(DOWNLOAD_URL)){
+            //可以通过url判断是否是指定任务的回调
+        }
+        final int p = task.getPercent();  //任务进度百分比
+        final String speed = task.getConvertSpeed();  //转换单位后的下载速度，单位转换需要在配置文件中打开
+        final long speed1 = task.getSpeed(); //原始byte长度速度
+
+        runOnUiThread(new Runnable() {
+
             @Override
-            public void onClick(View view) {
-                Aria.download(this).load(DOWNLOAD_URL).pause();
+            public void run() {
+
+                mPb.setProgress(p);
+                mSize.setText(speed);
+                mSpeed.setText(speed1/1024+"mb");
+
             }
         });
 
-        //删除任务
-        view.findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Aria.download(this).load(DOWNLOAD_URL).cancel();
-            }
-        });
+
+
     }
 
-    @Override protected void onResume() {
-        super.onResume();
-        Aria.download(this).addSchedulerListener(new MySchedulerListener());
+    @Download.onTaskComplete void taskComplete(DownloadTask task) {
+        //在这里处理任务完成的状态
     }
 
-    private class MySchedulerListener extends Aria.DownloadSchedulerListener {
-
-        @Override public void onTaskStart(DownloadTask task) {
-
-            mSize.setText(task.getConvertFileSize());
-        }
-
-        @Override public void onTaskStop(DownloadTask task) {
-            Toast.makeText(MainActivity.this, "停止下载", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override public void onTaskCancel(DownloadTask task) {
-            Toast.makeText(MainActivity.this, "取消下载", Toast.LENGTH_SHORT).show();
-            mSize.setText("0m");
-            mSpeed.setText("");
-            mPb.setProgress(0);
-        }
-
-        @Override public void onTaskFail(DownloadTask task) {
-            Toast.makeText(MainActivity.this, "下载失败", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override public void onTaskComplete(DownloadTask task) {
-            Toast.makeText(MainActivity.this, "下载完成", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override public void onTaskRunning(DownloadTask task) {
-            mSpeed.setText(task.getConvertSpeed());
-            mPb.setProgress(task.getPercent());
+    @Download.onTaskFail void taskFail(DownloadGroupTask task) {
+        if(task.getKey().equals(DOWNLOAD_URL)){
+            //任务失败时的注解，任务执行失败时进行回调
+            System.out.println("luokun1："+task.getExtendField());
         }
     }
-
 }
