c7a0f5d9b5406444977f3ef001bf3568c2d03f6a
罗坤 <1498710037@qq.com>
2020-03-18 15:33:21 +0800

    replace "aria" to "liulishuo.filedownloader"

--- aria-download/src/main/java/com/github/arialyy/ariatest/MainActivity.java
+++ aria-download/src/main/java/com/github/arialyy/ariatest/MainActivity.java
@@ -1,118 +1,127 @@
 package com.github.arialyy.ariatest;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import com.arialyy.annotations.Download;
-import com.arialyy.aria.core.Aria;
-import com.arialyy.aria.core.task.DownloadGroupTask;
-import com.arialyy.aria.core.task.DownloadTask;
-import com.arialyy.aria.util.RecordUtil;
-
-import static com.arialyy.aria.util.CheckUtil.checkDPathConflicts;
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadLargeFileListener;
+import com.liulishuo.filedownloader.FileDownloader;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String DOWNLOAD_URL =
             "http://static.gaoshouyou.com/d/d4/4f/d6d48db3794fb9ecf47e83c346570881.apk";
-    long taskId;
-    View main;
-    HorizontalProgressBarWithNumber mPb;
-    TextView mSize;
-    TextView mSpeed;
+    String filePath = Environment.getExternalStorageDirectory().getPath() + "/aria_test.apk";
+    BaseDownloadTask taskId;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main2);
+        final View main = LayoutInflater.from(this).inflate(R.layout.activity_main2, null);
+        final HorizontalProgressBarWithNumber mPb = (HorizontalProgressBarWithNumber) main.findViewById(R.id.progressBar);
+        final TextView mSize = (TextView) main.findViewById(R.id.size);
+        final TextView mSpeed = (TextView) main.findViewById(R.id.speed);
 
-        //注册aria
-        Aria.download(this).register();
-        Aria.get(this).getDownloadConfig().setConvertSpeed(true);
+        //创建任务
+        FileDownloader.setup(this);
 
-        main = getLayoutInflater().inflate(R.layout.activity_main2, null);
-        mPb = (HorizontalProgressBarWithNumber) main.findViewById(R.id.progressBar);
-        mSize = (TextView) main.findViewById(R.id.size);
-        mSpeed = (TextView) main.findViewById(R.id.speed);
+        taskId = FileDownloader.getImpl().create(DOWNLOAD_URL)
+                .setPath(filePath)
+                .addHeader("Accept-Encoding","identity")
+                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36")
+                .setListener(new FileDownloadLargeFileListener() {
+                    @Override
+                    protected void pending(BaseDownloadTask task, long soFarBytes, long totalBytes) {
+                        System.out.println("luokun:pending "+taskId+", "+task);
+                    }
+
+
+                    @Override
+                    protected void connected(BaseDownloadTask task, String etag, boolean isContinue, int soFarBytes, int totalBytes) {
+                        System.out.println("luokun:connected "+taskId+", "+task);
+                    }
+
+                    @Override
+                    protected void progress(BaseDownloadTask task, final long soFarBytes, final long totalBytes) {
+
+//                                        mSize.setText((int)(soFarBytes/totalBytes));
+//                                        mSpeed.setText(speed1/1024+"mb");
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                System.out.println("luokun:progress, soFarBytes: "+soFarBytes+", totalBytes: "+totalBytes);
+                                mPb.setProgress((int)(soFarBytes/totalBytes));
+                                mSpeed.setText("luokun");
+                                main.invalidate();
+                                main.requestLayout();
+                            }
+                        });
+
+                    }
+
+
+                    @Override
+                    protected void blockComplete(BaseDownloadTask task) {
+                        System.out.println("luokun:blockComplete "+taskId+", "+task);
+                    }
+
+                    @Override
+                    protected void paused(BaseDownloadTask task, long soFarBytes, long totalBytes) {
+                        System.out.println("luokun:paused, soFarBytes: "+soFarBytes+", totalBytes: "+totalBytes);
+                    }
+
+                    @Override
+                    protected void completed(BaseDownloadTask task) {
+                        System.out.println("luokun:completed "+taskId+", "+task);
+                    }
+
+                    @Override
+                    protected void error(BaseDownloadTask task, Throwable e) {
+                        System.out.println("luokun:error "+taskId+", "+task);
+                    }
+
+                    @Override
+                    protected void warn(BaseDownloadTask task) {
+                        System.out.println("luokun:warn "+taskId+", "+task);
+                    }
+                });
+
 
     }
 
+    /**
+     * isUsing 表示以前有下载过，存在缓存的断点文件
+     * isRunning 为true时不允许#start() #reuse()
+     * **/
     public void onClick(View v){
         if(v.getId()==R.id.start){
-            //创建任务
-            String filePath = Environment.getExternalStorageDirectory().getPath() + "/aria_test.apk";
-//            System.out.println("luokun"+checkDPathConflicts(false,filePath));
-//            int state = Aria.download(this)
-//                    .load(taskId).getTaskState();
-//            System.out.println("luokun state"+state);
-//            if(state == 3){
-//                Aria.download(this)
-//                        .load(taskId)
-//                        .reStart();
+            System.out.println("luokunm:"+Environment.getExternalStorageDirectory().getPath() + "/aria_test.apk");
+            if(!taskId.isRunning()){
+                if(taskId.isUsing()){
+                    taskId.reuse();
+                }
+                taskId.start();
+            }
+
+//            if(!taskId.isRunning()&&taskId.pause()){ //继续
+//                taskId.reuse();
+//                taskId.start();
 //            }
 
-            if(checkDPathConflicts(false,filePath)){
-                taskId = Aria.download(this)
-                        .load(DOWNLOAD_URL)
-                        .setFilePath(filePath) //设置文件保存的完整路径
-                        .create();
-            }else {
-                Aria.download(this)
-                        .load(RecordUtil.getTaskRecord())
-                        .resume();
-            }
-
-        }else if(v.getId()==R.id.stop){
-
-            Aria.download(this)
-                    .load(taskId)
-                    .stop();
-
-        }else if(v.getId()==R.id.cancel){
-            Aria.download(this)
-                    .load(taskId)
-                    .cancel();
-        }
-    }
-
-    //在这里处理任务执行中的状态，如进度进度条的刷新
-    @Download.onTaskRunning protected void running(DownloadTask task) {
-        if(task.getKey().equals(DOWNLOAD_URL)){
-            //可以通过url判断是否是指定任务的回调
-        }
-        final int p = task.getPercent();  //任务进度百分比
-        final String speed = task.getConvertSpeed();  //转换单位后的下载速度，单位转换需要在配置文件中打开
-        final long speed1 = task.getSpeed(); //原始byte长度速度
-
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                mPb.setProgress(p);
-                mSize.setText(speed);
-                mSpeed.setText(speed1/1024+"mb");
-
-            }
-        });
-
-
-
-    }
-
-    @Download.onTaskComplete void taskComplete(DownloadTask task) {
-        //在这里处理任务完成的状态
-    }
-
-    @Download.onTaskFail void taskFail(DownloadGroupTask task) {
-        if(task.getKey().equals(DOWNLOAD_URL)){
-            //任务失败时的注解，任务执行失败时进行回调
-            System.out.println("luokun1："+task.getExtendField());
+        }else if(v.getId()==R.id.stop){//暂停
+            taskId.pause(); // return true表示暂停成功 false表示暂停失败（可能已经暂停了，然后又按了一次）
+        }else if(v.getId()==R.id.cancel){//取消并清除temp文件
+            FileDownloader.getImpl().clear(taskId.getId(),filePath);
         }
     }
 }
