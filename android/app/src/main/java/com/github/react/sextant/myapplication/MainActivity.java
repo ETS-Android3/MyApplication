a13b6ebe60b1ba1b40bd6d862291bcd874d10c44
罗坤 <1498710037@qq.com>
2021-01-21 13:46:55 +0800

    ⭐️集成React-Native到现有原生应用⭐️

--- android/app/src/main/java/com/github/react/sextant/myapplication/MainActivity.java
+++ android/app/src/main/java/com/github/react/sextant/myapplication/MainActivity.java
@@ -1,15 +1,16 @@
 package com.github.react.sextant.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, com.github.react.sextant.fragment.CrimeActivity.class);
+        Intent intent = new Intent(this, com.github.react.sextant.myapplication.MyReactActivity.class);
         startActivity(intent);
     }
 }
